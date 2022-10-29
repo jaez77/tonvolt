17eacff1b46f09ed8ec68c623e6d57585b2aae1f
Jaeyoung Lee <jaez@tonvolt.com>
2022-10-30 03:22:26 +0900

    Update vcard

--- src/routes/namecode/op85adhaei/+page.svelte
+++ src/routes/namecode/op85adhaei/+page.svelte
@@ -1,20 +1,7 @@
 <script>
-  // import vCardsJS from "vcard-js";
+  import { onMount } from "svelte";
 
-  // // This is your vCard instance, that
-  // // represents a single contact file
-  // let vCard = vCardsJS();
-
-  // // Set contact properties
-  // vCard.firstName = "Jaeyoung";
-  // vCard.lastName = "Lee";
-  // vCard.organization = "Tonvolt";
-  // vCard.title = "Programmer";
-  // vCard.email = "jaez@tonvolt.com";
-  // vCard.cellPhone = "+82 10-9854-8759";
-
-  // // Save contact to VCF file
-  // vCard.saveToFile(`jaez.vcf`);
+  onMount(() => {
+    location.href = "op85adhaei/Jaeyoung.vcf";
+  });
 </script>
-
-<a href="op85adhaei/Jaeyoung.vcf">vcf</a>
